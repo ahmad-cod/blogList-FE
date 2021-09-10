e8240a9365346ee72594449b89e0bad3b44b8c43
aroyehunahmad <ahmadadewale800@gmail.com>
2021-09-10 17:03:01 +0100

    Store the information about the signed in user in the Redux store.

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,25 @@
+
+const usersReducer = (state = null, action) => {
+  const { type, data } = action
+  switch (type) {
+  case 'SET_USER':
+    return data
+  case 'REMOVE_USER':
+    return null
+  default:
+    return state
+  }
+}
+
+export const setUser = data => {
+  return {
+    type: 'SET_USER',
+    data
+  }
+}
+
+export const removeUser = {
+  type: 'REMOVE_USER'
+}
+
+export default usersReducer
