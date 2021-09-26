651732664ee3fbc9933159298dfff2b79b406f9f
aroyehunahmad <ahmadadewale800@gmail.com>
2021-09-26 09:00:56 +0100

    Style Listing of Blogs.

--- src/components/blogs/Blog.js
+++ src/components/blogs/Blog.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {
-  List,
-  ListItem,
-  ListIcon,
-  MdCheckCircle
+  Box,
+  Text,
+  Heading,
+  Avatar,
+  Flex
 } from '@chakra-ui/react'
+import { StarIcon } from '@chakra-ui/icons'
 
 // const blogStyle = {
 //   paddingTop: 10,
@@ -16,16 +18,48 @@ import {
 //   margin: '5px 2px'
 // }
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, user, index }) => {
   return (
-    <List spacing={8}>
-      <ListItem spacing={4}>
-        <ListIcon as={MdCheckCircle} color="green.500" />
-        <Link to={`/blogs/${blog.id}`}>
-          {blog.title} by {blog.author}
-        </Link>
-      </ListItem>
-    </List>
+    <Flex my='14px'>
+      <Box
+        mr='16px'
+        color='rgba(230, 230, 230, 1)'
+        fontWeight='bold'
+        lineHeight='36px'
+        fontSize='30px'
+      >0{index + 1}</Box>
+      <Box>
+        <Flex alignItems='center' my='8px'>
+          <Avatar size='xs' name={user.name}></Avatar>
+          <Heading
+            pl='8px'
+            fontSize='13px'
+            lineHeight='17px'
+            fontWeight='500'
+          >{user.name}</Heading>
+        </Flex>
+        <Heading
+          as='h2'
+          my='4px'
+          fontWeight='700'
+          fontSize='16px'
+          color='rgba(41, 41, 41, 1)'
+          lineHeight='20px'
+        >
+          <Link to={`/blogs/${blog.id}`}>
+            {blog.title}
+          </Link>
+        </Heading>
+        <Box
+          color='rgba(117, 117, 117, 1)'
+          fontSize='13px'
+          lineHeight='20px'>
+          <Text as='span' size='xs'>Sep 15. </Text>
+          <Text as='span' size='2xs' mr='8px'> 4 min read </Text>
+          {blog.likes > 10 ? <StarIcon /> : ''}
+        </Box>
+      </Box>
+    </Flex>
   )
 }
 
--- src/components/blogs/BlogList.jsx
+++ src/components/blogs/BlogList.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import Blog from './Blog'
-
+import {
+  Container,
+  Box,
+  Flex,
+  Text
+} from '@chakra-ui/react'
+import { BellIcon } from '@chakra-ui/icons'
 const BlogList = () => {
   const blogs = useSelector(state => state.blogs)
   const user = useSelector(state => state.user)
@@ -9,8 +15,25 @@ const BlogList = () => {
     return <p>No blog</p>
   }
 
-  return blogs.sort((a, b) => b.likes - a.likes)
-    .map(blog => <Blog key={blog.id} blog={blog} user={user} />)
+  return (
+    <Container>
+      <Flex>
+        <BellIcon color='gray.500' mr='10px'/>
+        <Text
+          fontWeight='700'
+          letterSpacing='0.083em'
+          fontSize='12px'
+          lineHeight='16px'
+          color='rgba(41, 41, 41, 1)'
+        >TRENDING ON BLOGGS</Text>
+      </Flex>
+      <Box>
+        { blogs.sort((a, b) => b.likes - a.likes)
+          .map((blog, index) => <Blog key={blog.id} blog={blog} index={index} user={user} />)
+        }
+      </Box>
+    </Container>
+  )
 }
 
 export default BlogList
